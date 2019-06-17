b437e1eb119d4826aba0ad8dd4791ff8b37e00b1
eduardogmisiuk <eduardogmisiuk@gmail.com>
2019-06-17 20:02:27 -0300

    Moves ArgumentHandler instantiation to AlgorithmRunner constructor

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-import src.arguments as ah
 from src.algorithms.runner import AlgorithmRunner
 
 def main():
-  arg_handler = ah.ArgumentHandler()
-
-  algorithm = arg_handler.get_algorithm()
-  image     = arg_handler.get_image()
-  path      = arg_handler.get_path()
-
-  ar = AlgorithmRunner(algorithm, image, path)
-  ar.run()
+	ar = AlgorithmRunner()
+	ar.run()
 
 if __name__ == "__main__":
-  main()
+	main()
